7836b46bcc0a69b66f506b925ccee640fd6a03c6
alikvkli <ali.kavakli4598@gmail.com>
2023-06-30 09:20:06 +0300

    update todos table types

--- database/migrations/2023_06_29_102349_create_todos_table.php
+++ database/migrations/2023_06_29_102349_create_todos_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->integer("user_id");
             $table->text("action");
-            $table->integer("status")->default(0);
+            $table->char("status")->default("0");
             $table->timestamps();
         });
     }
